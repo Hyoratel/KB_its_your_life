d9a84fb0ef9ab01203d9deb15743144cf55bdd57
전효빈 <wjsgyqls95@gmail.com>
2025-04-04 11:36:56 +0900

    07심화(심화) 추가/ 교재 내용과 다르게 나옴

---
+++ vue/07_vue/07_vue(심화)/provide-inject-test/src/components/SongList.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul>
+    <SongListItem v-for="s in songs" :key="s.id" :song="s" />
+  </ul>
+  <div>체크된 곡 수 : {{ doneCount }}</div>
+</template>
+<script>
+import SongListItem from './SongListItem.vue';
+export default {
+  name: 'SongList',
+  components: {
+    SongListItem,
+  },
+  props: ['songs'],
+  inject: ['icons', 'doneCount'],
+  mounted() {
+    console.log('아이콘:', this.icons, '현재 song:', this.song);
+  },
+};
+</script>
