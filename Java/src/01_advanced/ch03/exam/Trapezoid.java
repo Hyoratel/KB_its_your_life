5bb1a87ab6b858e25af7a8f17d1ad4238491a0fb
전효빈 <wjsgyqls95@gmail.com>
2025-04-16 16:50:24 +0900

    01_advanced clear

---
+++ Java/src/01_advanced/ch03/exam/Trapezoid.java
@@ -0,0 +1,12 @@
+package ch03.exam;
+
+//사다리꼴 넓이
+public class Trapezoid {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int c = 7;
+        double extent = (a+b)*c/2.0;
+        System.out.println(extent);
+    }
+}
